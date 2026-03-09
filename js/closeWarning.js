fc0a12a77493f932fd9a8247f07530e9eba308bf
Kristian Cernak <cernak.kiko@gmail.com>
2026-03-09 21:24:58 +0100

    -Add warning message -Get prepared png for og:image

--- js/closeWarning.js
+++ js/closeWarning.js
@@ -1,9 +1,7 @@
 document.querySelector('.btn-agree').addEventListener('click', function() {
-    // Nájdeme hlavný obal a pridáme mu triedu pre animáciu
     const warningOverlay = document.querySelector('.warning');
     warningOverlay.classList.add('dismiss');
 
-    // Voliteľné: Úplné odstránenie z DOM po skončení animácie (kvôli čistote kódu)
     warningOverlay.addEventListener('transitionend', () => {
         warningOverlay.remove();
     }, { once: true });
